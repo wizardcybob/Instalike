7a1b3fbdefbf2c93571a3e80ac00641b90bdec11
Chloé JACOB <chloe.jacob4@etu.unistra.fr>
2023-02-27 15:45:53 +0100

    automatiser le like sur la vue 'post/X'

--- src/redux/post/reducer.ts
+++ src/redux/post/reducer.ts
@@ -2,6 +2,7 @@ import { Instalike } from '@jmetterrothan/instalike';
 import { Reducer } from 'redux';
 
 import { PostAction, SET_POST } from './actions';
+import { SetLikeFeedAction, SetUnlikeFeedAction, LIKE_POST_FEED, UNLIKE_POST_FEED } from '../feed/actions'
 
 type PostState = {
   data?: Instalike.Post;
@@ -11,10 +12,20 @@ const intialState: PostState = {
   data: undefined,
 };
 
-const postReducer: Reducer<PostState, PostAction> = (state = intialState, action) => {
+const postReducer: Reducer<PostState, PostAction | SetLikeFeedAction | SetUnlikeFeedAction> = (state = intialState, action) => {
   switch (action.type) {
     case SET_POST:
       return { ...state, data: action.payload };
+    case LIKE_POST_FEED:
+      if(state.data) {
+        return {...state, data: {...state.data, viewerHasLiked: true, likesCount: state.data.likesCount + 1 } };
+      }
+      return state;
+    case UNLIKE_POST_FEED:
+      if(state.data) {
+        return {...state, data: {...state.data, viewerHasLiked: false, likesCount: state.data.likesCount - 1 } };
+      }
+      return state;
     default:
       return state;
   }
